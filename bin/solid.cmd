9395c77737f9bb2e3958529f8e975dc442ff99bc
rimmartin <mathmldashx@yahoo.com>
2018-11-25 21:59:17 -0500

    Update solid.cmd
    removed comments

--- bin/solid.cmd
+++ bin/solid.cmd
@@ -4,9 +4,5 @@
         exit /b %errorlevel%
 :} */0;
 
-//process.stdin.setRawMode(true);
-//process.stdin.resume();
-//process.stdin.on('data', process.exit.bind(process, 0));
-
 const startCli = require('./lib/cli')
 startCli()
